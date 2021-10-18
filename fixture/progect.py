0111f9a068564b9f1525b36c17c0b4e1951fa8cf
Vladimir-Baran <Allen1Walker@yandex.ru>
2021-10-18 19:15:58 +0300

    Задания 25

--- fixture/progect.py
+++ fixture/progect.py
@@ -1,8 +1,6 @@
 import time
 import string
 import random
-from model.progect import Progect
-import itertools
 
 class ProgectHelper:
 
@@ -24,21 +22,6 @@ class ProgectHelper:
             list_progect.append(name)
         return list_progect
 
-    # def get_progect_list_tr2(self):
-    #     wd = self.app.wd
-    #     self.open_manage_projects_page()
-    #     tr = wd.find_elements_by_css_selector("tr.row-2")
-    #     list_progect_2 = []
-    #     for element in tr:
-    #         cells = element.find_elements_by_tag_name("td")
-    #         name = cells[0].text
-    #         list_progect_2.append(name)
-    #     return list_progect_2
-    #
-    # def get_progect_list(self):
-    #     wd = self.app.wd
-    #     return "\n".join(map(str, itertools.chain.from_iterable(zip(self.get_progect_list_tr1(), self.get_progect_list_tr2()))))
-
     def open_manage_projects_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Manage").click()
@@ -53,14 +36,15 @@ class ProgectHelper:
         rand = self.random_string("asd", 10)
         return rand
 
-    def add_progect(self, ):
+    def add_progect(self, random_name):
         wd = self.app.wd
-        wd.find_element_by_name("name").send_keys(self.random_name())
+        wd.find_element_by_name("name").send_keys(random_name)
         wd.find_element_by_xpath("//input[@value='Add Category']").click()
         time.sleep(1)
 
     def del_progect(self):
         wd = self.app.wd
+
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.find_element_by_xpath("//input[@value='Delete Category']").click()
         time.sleep(6)
